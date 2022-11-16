59854258a551c0f911e38b40cb439ecd9c57b7fd
kedaragate <100003176+kedaragate@users.noreply.github.com>
2022-11-16 19:46:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const navLinksContainer = document.getElementsByClassName(
   "nav-links-container"
 )[0];
 hamburgerMenu.addEventListener("click", () => {
- navLinksContainer.style.transform=(navLinksContainer.style.transform=="translateX(-200px)")?
-"translateX(200px)":"translateX(-200px)";
+ navLinksContainer.style.transform=(navLinksContainer.style.transform=="translateX(-150px)")?
+"translateX(150px)":"translateX(-150px)";
 });
 
 
