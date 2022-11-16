ca74308da50c6adb9ba23d808a794db78a57e0d7
Kedar Agate <kedar.agate@gmail.com>
2022-11-16 09:05:20 +0530

    style

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ exploreIcon.addEventListener("mouseover", () => {
 exploreIcon.addEventListener("mouseout", () => {
   exploreIcon.classList.remove("fa-flip");
 });
+
+const hamburgerMenu = document.getElementsByClassName("hamburger-menu")[0];
+const navLinksContainer = document.getElementsByClassName(
+  "nav-links-container"
+)[0];
+hamburgerMenu.addEventListener("click", () => {
+  navLinksContainer.style.right = "0";
+});
