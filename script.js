9f53e97fbda0a71de6fb88a67e872b32a8f055cc
kedaragate <100003176+kedaragate@users.noreply.github.com>
2022-11-16 13:58:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const navLinksContainer = document.getElementsByClassName(
   "nav-links-container"
 )[0];
 hamburgerMenu.addEventListener("click", () => {
- return navLinksContainer.style.transform="none"
+ return navLinksContainer.style.right="0"
 
 }
 
