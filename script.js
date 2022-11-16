fcea4d5b3cb83b3a9f4fe981f390f63204d90997
kedaragate <100003176+kedaragate@users.noreply.github.com>
2022-11-16 13:48:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-const exploreIcon = document.getElementsByClassName("fa-magnifying-glass")[0];
 
-exploreIcon.addEventListener("mouseover", () => {
-  exploreIcon.classList.add("fa-flip");
-});
-exploreIcon.addEventListener("mouseout", () => {
-  exploreIcon.classList.remove("fa-flip");
-});
 
 const hamburgerMenu = document.getElementsByClassName("hamburger-menu")[0];
 const navLinksContainer = document.getElementsByClassName(
   "nav-links-container"
 )[0];
 hamburgerMenu.addEventListener("click", () => {
- return navLinksContainer.style.transform=(navLinksContainer.style.transform=="translateX(-200%)")?"none":
-"translateX(-200%)";
+ return navLinksContainer.style.transform="translateX(200%)"
+
 }
 
 
