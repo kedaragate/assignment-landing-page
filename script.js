56458a0b3b0cc20c2024e1557cc3d9d6719493ed
Kedar Agate <kedar.agate@gmail.com>
2022-11-17 09:49:21 +0530

    btn

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-
-
 const hamburgerMenu = document.getElementsByClassName("hamburger-menu")[0];
 const navLinksContainer = document.getElementsByClassName(
   "nav-links-container"
 )[0];
 hamburgerMenu.addEventListener("click", () => {
- navLinksContainer.style.transform=(navLinksContainer.style.transform=="translateX(-200px)")?
-"translateX(200px)":"translateX(-200px)";
+  //   navLinksContainer.style.transform =
+  //     navLinksContainer.style.transform == "translateX(200px)"
+  //       ? "translateX(-200px)"
+  //       : "translateX(200px)";
+  navLinksContainer.style.visibility =
+    navLinksContainer.style.visibility == "hidden" ? "visible" : "hidden";
 });
-
-
